42b3695912a0f5a3e47365474c9148e865c463f3
gineude <gineude@gmail.com>
2022-09-03 09:50:20 -0300

    Entendo o coneito de flags.

---
+++ executando-regex/flags.js
@@ -0,0 +1,11 @@
+/*
+    conceito de flags
+
+    g -> global
+    i -> ignore case
+*/
+
+const texto = 'Carlos assinou o abaixo-assinado.';
+console.log(texto.match(/C|ab/)); // sem flag
+console.log(texto.match(/c|ab/i)); // com flag i
+console.log(texto.match(/ab|c/gi)); // com flag g e i
