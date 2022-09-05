eb1f533acf4a2394c5dfa47356e55bee5581edae
gineude <gineude@gmail.com>
2022-09-05 07:42:02 -0300

    Alguns cuidados

---
+++ caracteres/AlgunsCuidados.js
@@ -0,0 +1,13 @@
+
+// delimitando com aspas simples
+const textoUmaLinha = '...';
+
+// delimitando com crase | back tick
+const textoMultiLinha = `
+    linha 1
+    linha 2
+`
+
+// cuidado com tab!
+// configurar a IDE para usar tab ao invés de espaços
+console.log('	'.match(/\s/g));
